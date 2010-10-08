a19a0d9c06aa416cbb5607bba01fd943a0599938
Edmund von der Burg <evdb@ecclestoad.co.uk>
2010-10-08 17:45:25 +0100

    Created admin area and trivial access control.

---
+++ lib/GrotSpot/Controller/Admin.pm
@@ -0,0 +1,36 @@
+package GrotSpot::Controller::Admin;
+
+use strict;
+use warnings;
+use parent 'Catalyst::Controller';
+
+sub auto : Private {
+    my ( $self, $c ) = @_;
+
+    # switch off analytics for this section of the site
+    $c->stash->{grotspot}{show_analytics} = 0;
+
+    # check that the user is allowed in here
+    my $expected_secret = 'stinkypops';
+    my $actual_secret                     #
+      = $c->req->param('admin_secret')    # get off url
+      || $c->session->{admin_secret}      # load from session
+      || '';                              # none
+
+    if ( $actual_secret ne $expected_secret ) {
+        $c->res->body('please provide the admin secret');
+        $c->res->status(200);
+        return 0;
+    }
+
+    # store the secret in the session for the next visit
+    $c->session->{admin_secret} = $actual_secret;
+
+    return 1;
+}
+
+sub index : Path : Args(0) {
+    my ( $self, $c ) = @_;
+}
+
+1;
