c45d6597e0fec1d953f045383275c50252e86518
Edmund von der Burg <evdb@ecclestoad.co.uk>
2010-10-09 16:55:03 +0100

    Reorganised the controller to be more extendable

--- lib/GrotSpot/Controller/Area.pm
+++ lib/GrotSpot/Controller/Area.pm
@@ -4,19 +4,31 @@ use strict;
 use warnings;
 use parent 'Catalyst::Controller';
 
-sub index : Path : Args(0) {
+sub base : PathPart('area') Chained('/') CaptureArgs(0) {
     my ( $self, $c ) = @_;
-    $c->res->redirect( $c->uri_for('/in/london') );
 }
 
-sub rate : Path : Args(1) {
+sub index : PathPart('') Chained('base') Args(0) {
+    my ( $self, $c ) = @_;
+    $c->res->redirect( $c->uri_for('/area/london') );
+}
+
+sub load_area : PathPart('') Chained('base') CaptureArgs(1) {
     my ( $self, $c, $area_code ) = @_;
+
+    # clean up the code
+    $area_code =~ s{\W+}{}g;
 
     # check that the area code exists in the db
     my $area = $c->model('DB::Area')->find( { code => $area_code } )
-      || die;
+      || $c->detach('/page_not_found');
 
     $c->stash->{area} = $area;
+}
+
+sub rate : PathPart('') Chained('load_area') Args(0) {
+    my ( $self, $c ) = @_;
+    my $area = $c->stash->{area};
 
     # get the point that we should rate
     my ( $lat, $lng ) = $area->get_random_lat_lng();
